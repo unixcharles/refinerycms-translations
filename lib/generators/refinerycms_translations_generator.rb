03a7e6729fd5a8c0f6f07452d92d85f1d90b03d0
Charles Barbier <unixcharles@gmail.com>
2011-05-26 18:28:53 +0200

    Fix generators to work with the latest RefineryCMS

--- lib/generators/refinerycms_translations_generator.rb
+++ lib/generators/refinerycms_translations_generator.rb
@@ -1,6 +1,8 @@
-class RefinerycmsTranslations < Refinery::Generators::EngineInstaller
-  
-  source_root File.expand_path('../../', __FILE__)
-  engine_name "translations"
-  
+require 'refinery/generators'
+
+class RefinerycmsTranslationsGenerator < ::Refinery::Generators::EngineInstaller
+
+  source_root File.expand_path('../../../', __FILE__)
+  engine_name 'refinerycms-translations'
+
 end
