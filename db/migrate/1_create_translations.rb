e67dba03790d83751834d9bee3638a6994f93f38
Charles Barbier <unixcharles@gmail.com>
2011-05-26 18:29:11 +0200

    Cleanup migration and seeds data

--- db/migrate/1_create_translations.rb
+++ db/migrate/1_create_translations.rb
@@ -18,14 +18,6 @@ class CreateTranslations < ActiveRecord::Migration
 
   def self.down
     UserPlugin.destroy_all({:name => "Translations"})
-
-    Page.find_all_by_link_url("/translations").each do |page|
-      page.link_url, page.menu_match = nil
-      page.deletable = true
-      page.destroy
-    end
-    Page.destroy_all({:link_url => "/translations"})
-
     drop_table :translations
   end
 
